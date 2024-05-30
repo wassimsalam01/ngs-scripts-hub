2fdd6599fa8baed5a93d96904150ab4a67923a2b
Wassim Salam <97744975+wassimsalam01@users.noreply.github.com>
2024-05-31 00:02:10 +0200

    Update removeReadsWithTile.py

--- fastq/removeReadsWithTile.py
+++ fastq/removeReadsWithTile.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # In this example, the tile 1105 scored poorly and needed to be removed. Replace 1105 with your tile number
 import re
-f=open("/home/wassim.salam/Functional/fastqScripts/nnn.fastq","r")
-n=open("/home/wassim.salam/Functional/fastqScripts/clean.fastq","a+")
+f=open("/path/to/pre-clean.fastq","r")
+n=open("/path/to/clean.fastq","a+")
 for i in f:
 	if re.match(".*:\d+:.*:\d:1105:.*:.*[01]:.:\d:.*",i) is not None:
 		i=f.next()
